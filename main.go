17dac49929c9d9b7dfcea7c0d3fdf89e735d5c15
Angshuk Nag <angshuk@gmail.com>
2026-08-25 19:08:29 +0530

    Added new handler and port selection from cmd args

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handleHealthzHTTPRequests(w http.ResponseWriter, req *http.Request) {
@@ -13,22 +14,35 @@ func handleHealthzHTTPRequests(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	const port = "8082"
+	const testPort = "8082"
+	const livePort = "8080"
 
-	fmt.Println("--- RUNNING ABSOLUTE LOCAL VERSION ---")
+	var portToUse string
+
+	if len(os.Args) > 1 {
+		arg := os.Args[1]
+		if arg == "live" {
+			portToUse = livePort
+		} else {
+			portToUse = testPort
+		}
+	} else {
+		portToUse = testPort
+	}
+
+	fmt.Println("--- RUNNING ABSOLUTE LOCAL VERSION !!!!! ---")
 	mux := http.NewServeMux()
-	// mux.HandleFunc("/healthz", handleHealthzHTTPRequests)
+	mux.HandleFunc("/healthz", handleHealthzHTTPRequests)
 
 	fileServerHandler := http.FileServer(http.Dir("."))
-	// mux.Handle("/app", http.StripPrefix("/app", fileServerHandler))
-	mux.Handle("/", fileServerHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
 
 	srv := &http.Server{
-		Addr:    ":" + port,
+		Addr:    ":" + portToUse,
 		Handler: mux,
 	}
 
 	
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", portToUse)
 	log.Fatal(srv.ListenAndServe())
 }
