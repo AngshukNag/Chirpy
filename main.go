1dee8d4ec2ae1fa8ac0caa390a634d93c13e3ea3
Angshuk Nag <angshuk@gmail.com>
2026-08-26 19:59:15 +0530

    Added fileserver hits count middleware and new endpoints for getting metrics and reseting hit counts

--- main.go
+++ main.go
@@ -7,17 +7,46 @@ import (
 	"os"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (apiCtx *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.path == "/reset" {
+			apiCtx.fileserverHits.Store(0)
+		} else {
+			apiCtx.fileserverHits.Add(1)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func handleHealthzHTTPRequests(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 }
 
+func handleMetricsAndResetHTTPRequest(w http.ResponseWriter, req *http.Request) {
+	fmt.Sprintf("Hits: %v", apiContext.fileserverHits.Load())
+	if req.URL.path == "/metrics" {
+		w.Write([]byte(fmt.Sprintf("Hits: %v", apiContext.fileserverHits.Load())))
+	}
+}
+
+var apiContext *apiConfig
+
+// func init() {
+// 	apiContext = &apiConfig{}
+// }
+
 func main() {
 	const testPort = "8082"
 	const livePort = "8080"
 
 	var portToUse string
+	apiContext = &apiConfig{}
 
 	if len(os.Args) > 1 {
 		arg := os.Args[1]
@@ -34,8 +63,11 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", handleHealthzHTTPRequests)
 
+	mux.HandleFunc("/metrics", handleMetricsAndResetHTTPRequest);
+	mux.HandleFunc("/reset", apiContext.middlewareMetricsInc(handleMetricsAndResetHTTPRequest));
+
 	fileServerHandler := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
+	mux.Handle("/app/", apiContext.middlewareMetricsInc(http.StripPrefix("/app", fileServerHandler)))
 
 	srv := &http.Server{
 		Addr:    ":" + portToUse,
